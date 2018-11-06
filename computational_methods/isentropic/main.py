59f3ae163e439181fea455fdde7316b9b9126815
cmolinaord <carlosmolina.ord@gmail.com>
2018-11-06 02:44:34 +0100

    Changing to try to debug

--- computational_methods/isentropic/main.py
+++ computational_methods/isentropic/main.py
@@ -2,20 +2,19 @@ import sys
 import numpy as np
 import tools
 import matplotlib.pyplot as plt
-
-import tmp
+from matplotlib.pyplot import Circle
 
 args = sys.argv
 ITERMAX = int(args[1])
 
 # Computational parameters
 delta = 0.005
-nx = 100
-ny = 50
+nx = 200
+ny = 200
 iter = 0
 
 # Domain parameters (m)
-L = 20
+L = 10
 H = 10
 
 # Physical constants
@@ -34,14 +33,14 @@ Vinput = 10
 
 # Creation of domain
 phi, dx, dy = tools.create_mesh(L, H, nx, ny) # Create a null matrix for phi
-phi = phi +1
-phi1 = phi + 0.1
-p = phi + p0
-T = phi + T0
-rho = phi + rho0
-Vx = phi + 0
-Vy = phi + 0
-V = phi + 0
+phi = np.zeros([nx,ny])
+phi1 = np.ones([nx,ny])
+p = np.ones([nx,ny]) + p0
+T = np.ones([nx,ny]) + T0
+rho = np.ones([nx,ny]) * p0
+Vx = np.ones([nx,ny])
+Vy = np.zeros([nx,ny])
+V = np.ones([nx,ny])
 
 x = np.linspace(0, L, nx)
 y = np.linspace(0, H, ny)
@@ -58,11 +57,10 @@ for i in range(0,ny):
 		dist = np.sqrt((x[j]-center[0])**2 + (y[i]-center[1])**2)
 		if dist < radius:
 			Solid[i,j] = True
-		#rho[i,j] = tools.density(p[i,j], T[i,j], R)
+			rho[i,j] = 0
 
 # Boundary conditions
-for i in range(0, ny): # rows
-	phi[i, 0] = 0
+phi[0, :] = 1
 
 # Iteration
 incr = 0.5
@@ -72,39 +70,38 @@ while incr > delta and iter < ITERMAX:
 	for i in range(0, ny): # rows
 		for j in range (0, nx): # columns
 			phi1[i,j], vn, vs, vw, ve = tools.calc_a(phi, rho, rho0, i, j, nx, ny, dx, dy, Vinput, Solid)
-
 			Vx[i,j] = 0.5*(vn + vs)
 			Vy[i,j] = 0.5*(ve + vw)
 			V[i,j]	= tools.np.sqrt(Vx[i,j]**2 + Vx[i,j]**2)
-
 			# Energy conservation (calculated temperature)
 			T[i,j] = T0 + 0.5*(Vinput**2 - V[i,j]**2)/c_p
 			# Isentropic condition (pressure calculated)
 			p[i,j] = p0 * (T[i,j]/T0)**(gamma/(gamma-1))
 			rho[i,j] = tools.density(p[i,j], T[i,j], R)
+	#phi1[:,0] = 1
 	incr = np.max(np.abs(phi1 - phi))
-	for i in range(0,ny):
-		for j in range(0,nx):
-			phi[i,j] = phi1[i,j]
+	phi[:,:] = phi1[:,:]
 	print("Iteration %i: maximum difference: %2.4e" %(iter, incr))
 
 
 fig1 = plt.figure()
 ax1 = fig1.gca()
-ax1.streamplot(xv, yv, Vx, Vy, density=[0.7, 2])
+ax1.streamplot(xv, yv, Vx, Vy, density=[0.5, 1])
+circ = Circle(center, radius)
+ax1.add_patch(circ)
+ax1.axis("equal")
 
 cmap = plt.get_cmap('PiYG')
 
 fig2 = plt.figure()
 ax2 = fig2.gca()
-im = ax2.pcolormesh(xv, yv, Vx, cmap=cmap)
+im = ax2.pcolormesh(xv, yv, V, cmap=cmap)
 fig2.colorbar(im, ax=ax2)
 
 fig3 = plt.figure()
 ax3 = fig3.gca()
-ax3.plot(x,phi[round(0.1*ny),:],'r-o')
-ax3.plot(x,phi[round(0.25*ny),:],'g-o')
-ax3.plot(x,phi[round(0.5*ny),:],'b-o')
-plt.show()
+ax3.plot(x,Vx[round(0.1*ny),:],'r-o')
+ax3.plot(x,Vx[round(0.25*ny),:],'g-o')
+ax3.plot(x,Vx[round(0.5*ny),:],'b-o')
 
-print(phi)
+plt.show()
